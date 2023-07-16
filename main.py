0e1174d63eb811c2116e7b89e193d41f79d335fa
Ivan Yastrebov <easy.quest@ya.ru>
2023-07-16 03:54:12 +0300

    Update: 16.07.2023 03:54:12

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/env python
 # -*- coding: utf-8 -*-
+import termux
 
 # Функция для вычисления площади прямоугольника;
 # от англ. calculate, «вычислять»
@@ -14,3 +15,6 @@ def calc_square(side_a, side_b):
 rectangle_area = calc_square(16, 9)
 
 print(rectangle_area)
+
+
+print(dir(termux.API))
