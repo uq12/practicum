60ecd5121619e5913b719f30014b95244a7ecb72
Ivan Yastrebov <easy.quest@ya.ru>
2023-07-17 08:51:51 +0300

    Update: 17.07.2023 08:51:51

---
+++ cracker-tool.sh
@@ -0,0 +1,45 @@
+#!/data/data/com.termux/files/usr/bin/env fish 
+ 
+# Cracker-Tool — Это инструмент на основе Python и Bash созданный для взлома и пентеста. 
+ 
+# Доступен следующий функционал: 
+# 1. IP-инструмент 
+# 2. Сканер субдоменов 
+# 3. Инструмент для Ddos-атак 
+# 4. Поиск админки 
+# 5. Has Cracker 
+# 6. Скачивание видео 
+# 7. SQL Injection 
+# 8. Конвертер текста в голос 
+# 9. Обфускатор Python 
+# 10. Набор для работы с Telegram 
+# 11. Termux Framework 
+# 12. Kali Nethunter Installer 
+# 13. Termux Tool 
+# 14. Смена URL 
+# 15. Сокращение ссылок 
+# 16. WEB-инструмент 
+# 17. Временная почта 
+# 18. Генератор Gmail 
+# 19. Генератор идентичности 
+# 20. Multi Ddos 
+# 21. Anon Share 
+# 22. Инструмент электронной почты 
+# 24. Инструмент CC 
+ 
+# Установка: 
+apt upgrade -y 
+apt update -y 
+termux-setup-storage 
+pkg install git  
+pkg install python -y 
+# git clone https://github.com/cracker911181/Cracker-Tool
+gcl cracker911181/Cracker-Tool
+
+ 
+# Запуск:
+# cd Cracker-Tool
+python cracker-main.py 
+cracker 
+
+
